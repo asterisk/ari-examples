e7d0e009e1b65d7d111c1c538df4bec84d006ec9
Matt Jordan <mjordan@digium.com>
2014-11-12 20:26:50 -0600

    Update bridge-dial/example.py
    Add some additional error handling, and dispose of bridges
    when either channel leaves the application.

--- bridge-dial/example.py
+++ bridge-dial/example.py
@@ -8,7 +8,9 @@ logging.basicConfig(level=logging.ERROR)
 
 client = ari.connect('http://localhost:8088', 'asterisk', 'asterisk')
 
+
 def safe_hangup(channel):
+    """Safely hang up the specified channel"""
     try:
         print "Hanging up %s" % channel.json.get('name')
         channel.hangup()
@@ -16,14 +18,25 @@ def safe_hangup(channel):
         if e.response.status_code != requests.codes.not_found:
             raise e
 
+
+def safe_bridge_destroy(bridge):
+    """Safely destroy the specified bridge"""
+    try:
+        bridge.destroy()
+    except requests.HTTPError as e:
+        if e.response.status_code != requests.codes.not_found:
+            raise e
+
+
 def stasis_start_cb(channel_obj, ev):
     """Handler for StasisStart"""
 
     channel = channel_obj.get('channel')
+    channel_name = channel.json.get('name')
     args = ev.get('args')
 
     if not args:
-        print "No arguments? No dial for you %s" % channel.json.get('name')
+        print "Error: {} didn't provide any arguments!".format(channel_name)
         return
 
     if args and args[0] != 'inbound':
@@ -31,7 +44,7 @@ def stasis_start_cb(channel_obj, ev):
         return
 
     if len(args) != 2:
-        print "%s didn't tell us who to dial" % channel.json.get('name')
+        print "Error: {} didn't tell us who to dial".format(channel_name)
         channel.hangup()
         return
 
@@ -39,27 +52,31 @@ def stasis_start_cb(channel_obj, ev):
 
     try:
         outgoing = client.channels.originate(endpoint=args[1],
-            app='bridge-dial', appArgs='dialed')
-    except requests.HTTPError as e:
+                                             app='bridge-dial',
+                                             appArgs='dialed')
+    except requests.HTTPError:
         print "Whoops, pretty sure %s wasn't valid" % args[1]
         channel.hangup()
         return
 
     channel.on_event('StasisEnd', lambda *args: safe_hangup(outgoing))
-    outgoing.on_event('ChannelDestroyed', lambda *args: safe_hangup(channel))
+    outgoing.on_event('StasisEnd', lambda *args: safe_hangup(channel))
 
     def outgoing_start_cb(channel_obj, ev):
         """StasisStart handler for our dialed channel"""
 
-        print "%s answered; bridging with %s" % (outgoing.json.get('name'),
-            channel.json.get('name'))
+        print "{} answered; bridging with {}".format(outgoing.json.get('name'),
+                                                     channel.json.get('name'))
         channel.answer()
 
         bridge = client.bridges.create(type='mixing')
         bridge.addChannel(channel=[channel.id, outgoing.id])
-        bridge.play(media='sound:tt-monkeys')
+
         # Clean up the bridge when done
-        outgoing.on_event('StasisEnd', lambda *args: bridge.destroy())
+        channel.on_event('StasisEnd', lambda *args:
+                         safe_bridge_destroy(bridge))
+        outgoing.on_event('StasisEnd', lambda *args:
+                          safe_bridge_destroy(bridge))
 
     outgoing.on_event('StasisStart', outgoing_start_cb)
 
@@ -67,4 +84,3 @@ def stasis_start_cb(channel_obj, ev):
 client.on_channel_event('StasisStart', stasis_start_cb)
 
 client.run(apps='bridge-dial')
-
